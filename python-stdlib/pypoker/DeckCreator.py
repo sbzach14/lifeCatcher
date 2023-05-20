313300774cf50a2289ac62d5907fdf7aba1db10a
sbzach14 <17750207510@163.com>
2023-05-20 15:14:18 +0800

    modify deck creator

--- python-stdlib/pypoker/DeckCreator.py
+++ python-stdlib/pypoker/DeckCreator.py
@@ -6,7 +6,7 @@ def pack_card(card_index):
 
 def Init_deck(initial_cards):
     deck_List = []
-    for card_index in range(initial_cards):
+    for card_index in initial_cards:
         deck_List.append(pack_card(card_index))
     
     return deck_List
