91523f85a3195f7942cf657165e4afde323071e3
sbzach14 <17750207510@163.com>
2023-05-29 16:44:46 +0800

    move all poker rule

--- Shuffle/GameRules/PokerBull.swift
+++ Shuffle/GameRules/PokerBull.swift
@@ -12,21 +12,11 @@ import PythonKit
 
 class PokerBull{
     static func FindWinner(inputCards:[Int], playerNum: Int) -> [Int]? {
-        let jsonString = """
-        {
-            "name": "John",
-            "age": 30,
-            "city": "New York"
-        }
-        """
         
         let sys = Python.import("sys")
         sys.path.append("/Users/naldochen/shuffle/python-stdlib/")
         let json = Python.import("json")
-        let jsonObj = json.loads(jsonString)
-        print(jsonObj["name"])
 
-        print("Start the function")
         let pokerBull =  Python.import("pypoker.PokerBull")
         print("import successfully")
         let pythonList = Python.list(inputCards)
@@ -35,10 +25,7 @@ class PokerBull{
         print(pokerBull)
         
         let pyobj =  Int(json.TestClass.TestFunc(playerNum))!
-        
-        print(pyobj)
-        print("////////////////////")
-        
+                
         let pythonObject =  pokerBull.PorkerBullGame.calResult(pythonList, pythonInt)
         print("Return Winner")
         // 使用 map() 函数将 PythonList 转换为 Int 数组
