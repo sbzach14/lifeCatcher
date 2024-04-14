b018577853deeddbd24ec7b69a8e6b18f51cc5dc
banana45 <675056807@qq.com>
2024-04-15 02:33:00 +0800

    111

--- Shuffle/Views/MainMenuView.swift
+++ Shuffle/Views/MainMenuView.swift
@@ -55,7 +55,7 @@ struct MainMenuView: View {
                     }
                 }
             }
-            .navigationBarTitle("")
+            .navigationBarTitle(" ")
             .background(
                 Image("bg")
                     .resizable()
